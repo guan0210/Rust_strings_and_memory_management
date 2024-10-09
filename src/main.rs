bb514d0a9efa2935be1df19a7ff34310c6638479
Nic McPhee <mcphee@morris.umn.edu>
2024-10-09 17:04:33 -0500

    Move `disemvowel` into its own module
    This moves the stub for the `disemvowel` function into its own module so the structure is more similar to that used in the C version of this project.
    
    We also cleaned up some weird import issues that probably date back to when Nic didn't really understand Rust that well.
    
    Co-authored-by: <Wenkai Guan guan0210@morris.umn.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
+pub mod disemvowel;
+
+use crate::disemvowel::disemvowel;
 use std::env;
 use std::fs;
 use std::path::Path;
-
-//The following crates are used for testing
-extern crate tempfile; //Creates temp files and directories
-use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*;
-use std::process::Command; // Run programs // Used for writing assertions
 
 fn main() {
     // This should be called with two command line arguments,
@@ -36,13 +33,9 @@ fn main() {
 fn read_file(path: &Path) -> String {
     fs::read_to_string(path).expect("Could not read the file")
 }
+
 fn write_file(path: &Path, s: &str) {
     fs::write(path, s).expect("Unable to write file");
-}
-
-//TODO: Return the input string without vowels.
-fn disemvowel(s: &str) -> String {
-    String::from(s)
 }
 
 // Everything from here down is Rust test code. You shouldn't need to
@@ -53,80 +46,31 @@ fn disemvowel(s: &str) -> String {
 // that up above and work to get the tests to pass. See the lab write-up
 // for some tips.
 
+// Tests that check that the correct panics are generated when
+// there aren't the correct number of command line arguments
+// or the input file isn't readable.
 #[cfg(test)]
 mod tests {
-    use super::*;
-    mod disemvowel {
-        use super::*;
-        #[test]
-        fn hello_world() {
-            let input = "Hello, world!";
-            let expected = "Hll, wrld!";
+    use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
+    use predicates::prelude::predicate;
+    use std::process::Command; // Run programs // Used for writing assertions
 
-            assert_eq!(expected, disemvowel(input));
-        }
-
-        #[test]
-        fn empty() {
-            assert_eq!("", disemvowel(""));
-        }
-
-        #[test]
-        fn no_vowels() {
-            assert_eq!("pqrst", disemvowel("pqrst"));
-        }
-
-        #[test]
-        fn all_vowels() {
-            assert_eq!("", disemvowel("aeiouAEIOUOIEAuoiea"));
-        }
-
-        #[test]
-        fn morris_minnesota() {
-            assert_eq!("Mrrs, Mnnst", disemvowel("Morris, Minnesota"));
-        }
-
-        #[test]
-        fn handle_punctuation() {
-            assert_eq!(
-                "n (nxplnd) lphnt!",
-                disemvowel("An (Unexplained) Elephant!")
-            );
-        }
-
-        #[test]
-        fn handle_unicode() {
-            assert_eq!("Sm hrglyphs: 𒐁	𒐌	𒐥	𒑳", disemvowel("Some hieroglyphs: 𒐁	𒐌	𒐥	𒑳"));
-            assert_eq!("Sm Lnr B: 	𐂀	𐂚	𐃃	𐃺", disemvowel("Some Linear B: 	𐂀	𐂚	𐃃	𐃺"));
-            assert_eq!(" lttl Phncn: 𐤀	𐤈	𐤔	𐤕", disemvowel("A little Phoenician: 𐤀	𐤈	𐤔	𐤕"));
-            assert_eq!(
-                "W cn hndl mj s wll! 🤣😃👍",
-                disemvowel("We can handle emoji as well! 🤣😃👍")
-            )
-        }
+    #[test]
+    fn requires_two_arguments() {
+        let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
+        cmd.arg("1");
+        cmd.assert()
+            .failure()
+            .stderr(predicate::str::contains("Not enough arguments"));
     }
 
-    // Tests that check that the correct panics are generated when
-    // there aren't the correct number of command line arguments
-    // or the input file isn't readable.
-    mod panic_tests {
-        use super::*;
-        #[test]
-        fn requires_two_arguments() {
-            let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
-            cmd.arg("1");
-            cmd.assert()
-                .failure()
-                .stderr(predicate::str::contains("Not enough arguments"));
-        }
-        #[test]
-        fn requires_read_file() {
-            let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
-            cmd.arg("/this/path/does/not/exist")
-                .arg("output/path/doesnt/matter");
-            cmd.assert()
-                .failure()
-                .stderr(predicate::str::contains("Could not read the file"));
-        }
+    #[test]
+    fn requires_read_file() {
+        let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
+        cmd.arg("/this/path/does/not/exist")
+            .arg("output/path/does/not/matter");
+        cmd.assert()
+            .failure()
+            .stderr(predicate::str::contains("Could not read the file"));
     }
 }
